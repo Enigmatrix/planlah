3637716843c5843802f54aa5d52c490dd7759780
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-05-14 23:20:07 +0800

    backend: add structure + swaggers

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "planlah.sg/backend/docs"
 )
 
 func main() {
 	srv := gin.Default()
-	srv.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
-	})
+
+	registerRoutes(srv)
+
+	// Swagger documentation
+	srv.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
 	srv.Run()
 }
