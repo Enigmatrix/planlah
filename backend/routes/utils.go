148b57b4e1f6738511ab2cda91eddb6262a1c47d
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-05-15 11:42:43 +0800

    rmv central .idea

---
+++ backend/routes/utils.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"io"
+	"net/http"
+)
+
+func Body[T any](ctx *gin.Context, dto *T) error {
+	err := ctx.ShouldBindJSON(&dto)
+	if err == nil {
+		return nil
+	}
+
+	// TODO make this only run in debug
+	var validationErrors validator.ValidationErrors
+
+	if errors.As(err, &validationErrors) {
+		validationMessage := make([]string, len(validationErrors))
+		for i, fieldError := range validationErrors {
+			validationMessage[i] = fieldError.Error()
+		}
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "validation", "validation": validationMessage})
+	} else if err == io.EOF {
+		message := fmt.Sprintf("Body of type %T expected", dto)
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "missingBody", "message": message})
+	} else {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "unknown", "message": err.Error()})
+	}
+	return err
+}
