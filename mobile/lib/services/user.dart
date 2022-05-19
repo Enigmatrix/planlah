cf6146b75eabbdf63685c2fa18316d7cb713a6d9
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-05-19 23:03:02 +0800

    feat(mobile): add initial user service

---
+++ mobile/lib/services/user.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+import 'package:mobile/model/user.dart';
+import 'package:mobile/services/auth.dart';
+
+class UserService extends GetConnect {
+  @override
+  void onInit() {
+    final auth = Get.find<AuthService>();
+
+    // TODO make this configurable
+    httpClient.baseUrl = "http://localhost:8080";
+
+    httpClient.addAuthenticator<dynamic>((request) async {
+      final token = await auth.user.value?.getIdToken();
+      if (token == null) return request;
+
+      final response = await post('/auth/verify', token);
+      Get.log('POST Verification of Data');
+      request.headers['Authorization'] = response.body['token'];
+      return request;
+    });
+
+    // retry token verification 3 times
+    httpClient.maxAuthRetries = 3;
+  }
+
+  Future<Response<UserInfo>> getInfo() async => await get<UserInfo>('/users/me/info');
+}
