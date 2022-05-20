2d499c91c4ee94821d4f7173a55d26ac8182de04
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-05-20 23:48:54 +0800

    feat(mobile): add initial user service

--- mobile/lib/services/user.dart
+++ mobile/lib/services/user.dart
@@ -14,7 +14,7 @@ class UserService extends GetConnect {
       final token = await auth.user.value?.getIdToken();
       if (token == null) return request;
 
-      final response = await post('/auth/verify', token);
+      final response = await post('/auth/verify', { "token": token });
       Get.log('POST Verification of Data');
       request.headers['Authorization'] = response.body['token'];
       return request;
