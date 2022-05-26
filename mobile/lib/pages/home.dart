4b7bc30dd0619570555bf576e801aad36a750539
JothamWong <jwys6747@gmail.com>
2022-05-27 01:02:03 +0800

    feat(mobile): Add navigation bar and placeholder pages for social feed, groups and profile

--- mobile/lib/pages/home.dart
+++ mobile/lib/pages/home.dart
@@ -3,40 +3,76 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobile/groups/pages/groups_page.dart';
+import 'package:mobile/pages/profile.dart';
+import 'package:mobile/pages/social_feed.dart';
 import 'package:mobile/services/auth.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageWidgetState();
+
+
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageWidgetState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    SocialFeedPage(),
+    GroupsPage(),
+    ProfilePage()
+  ];
+
+  var currentIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final AuthService auth = Get.find();
-
     return Scaffold(
-        appBar: AppBar(title: const Text("Home")),
-        body: ListView(
-            children: [
-          ElevatedButton(
-              onPressed: () async {
-                await auth.signOutFromGoogle();
-                await Get.offAndToNamed('signIn');
-              },
-              child: const Text("LOG OUT")),
-          ElevatedButton(
-              onPressed: () async {
-                log((await auth.user.value?.getIdToken()).toString());
-              },
-              child: const Text("PRINT TOKEN")),
-          ElevatedButton(
-              onPressed: () {
-                Get.to(() => const GroupsPage());
-              },
-              child: const Text("Groups"))
-        ]));
+      appBar: AppBar(
+        title: const Text("planlah"),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.group_add_outlined),
+            tooltip: "Create new group",
+          )
+        ],
+      ),
+      body: Center(
+          child: _pages.elementAt(_selectedIndex)
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.feed),
+            label: "Feed"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.groups),
+              label: "Groups"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Profile"
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
+
+
+
+
+
