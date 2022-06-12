5ae745a254ba150fcf852333aafa210c7a7c76ae
Jotham Wong <45916998+JothamWong@users.noreply.github.com>
2022-06-13 03:20:02 +0800

    feat(backend + mobile): Moved calls to initState() and added gender endpoint

--- mobile/lib/services/misc.dart
+++ mobile/lib/services/misc.dart
@@ -2,9 +2,12 @@ import 'package:get/get.dart';
 import 'package:mobile/services/base_connect.dart';
 
 class MiscService extends BaseConnect {
-  Future<Response<List<String>?>> getTowns() async => await get('/misc/towns',
-      decoder: decoderForListString());
+    Future<Response<List<String>?>> getTowns() async => await get('/misc/towns', 
+        decoder: decoderForListString());
 
-  Future<Response<List<String>?>> getGenders() async => await get('/misc/gender',
-      decoder: decoderForListString());
+    Future<Response<List<String>?>> getGenders() async => await get('/misc/gender',
+        decoder: decoderForListString());
+
+    Future<Response<List<String>?>> getFood() async => await get('/misc/food',
+        decoder: decoderForListString());
 }
