539ea212d1e2416d90e1dd02b16e4f94ce71097d
Jotham Wong <45916998+JothamWong@users.noreply.github.com>
2022-06-13 23:01:14 +0800

    feat(mobile): Added user sign up, snackbar message for existing user

--- mobile/lib/dto/user.dart
+++ mobile/lib/dto/user.dart
@@ -1,9 +1,21 @@
 class CreateUserDto {
   String name;
   String nickname;
+  String gender;
+  String town;
   String firebaseToken;
+  List<String?> attractions;
+  List<String?> food;
 
-  CreateUserDto(this.name, this.nickname, this.firebaseToken);
+  CreateUserDto(
+      this.name,
+      this.nickname,
+      this.gender,
+      this.town,
+      this.firebaseToken,
+      this.attractions,
+      this.food,
+  );
 
   Map<String, dynamic> toJson() => {
     'name': name,
