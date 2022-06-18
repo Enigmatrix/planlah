991c9cd33ba681c3ab9c0eb8fd9a40dda75257da
Jotham Wong <45916998+JothamWong@users.noreply.github.com>
2022-06-19 03:38:26 +0800

    feat(mobile): Filter for groups

--- mobile/lib/pages/groups_page.dart
+++ mobile/lib/pages/groups_page.dart
@@ -15,8 +15,19 @@ class GroupsPage extends StatefulWidget {
 }
 
 class _GroupsPageState extends State<GroupsPage> {
+
   // Replace with actual retrieving from database in the future
-  List<ChatGroup> groups = getChatGroups();
+  // The list of all groups by which we filter on
+  final List<ChatGroup> allGroups = getChatGroups();
+  // What's actually displayed
+  late List<ChatGroup> groups;
+
+  @override
+  initState() {
+    groups = allGroups;
+    super.initState();
+  }
+
   // Hard code for now
   static List<ChatGroup> getChatGroups() {
     const data = [
@@ -38,75 +49,11 @@ class _GroupsPageState extends State<GroupsPage> {
       },
       {
         "groupId": "12345",
-        "groupName": "Addicted to Coca-Cola",
-        "mostRecentText": "Coca-Cola is the number 1 soft drink in the world!",
+        "groupName": "Addicted to Pepsi-Cola",
+        "mostRecentText": "Pepsi-Cola is the worst soft drink in the world!",
         "time": "23:54",
-        "photoUrl": "https://www.foodnavigator-asia.com/var/wrbm_gb_food_pharma/storage/images/_aliases/wrbm_large/publications/food-beverage-nutrition/foodnavigator-asia.com/headlines/business/coca-cola-stands-firm-amid-criticism-of-human-rights-violations-in-south-east-asia/8624735-1-eng-GB/Coca-Cola-stands-firm-amid-criticism-of-human-rights-violations-in-South-East-Asia.jpg",
-        "description": "For people addicted to Coca-Cola"
-      },
-      {
-        "groupId": "10026",
-        "groupName": "Addicted to Coca-Cola",
-        "mostRecentText": "Coca-Cola is the number 1 soft drink in the world!",
-        "time": "23:54",
-        "photoUrl": "https://www.foodnavigator-asia.com/var/wrbm_gb_food_pharma/storage/images/_aliases/wrbm_large/publications/food-beverage-nutrition/foodnavigator-asia.com/headlines/business/coca-cola-stands-firm-amid-criticism-of-human-rights-violations-in-south-east-asia/8624735-1-eng-GB/Coca-Cola-stands-firm-amid-criticism-of-human-rights-violations-in-South-East-Asia.jpg",
-        "description": "For people addicted to Coca-Cola"
-      },
-      {
-        "groupId": "10027",
-        "groupName": "Addicted to Coca-Cola",
-        "mostRecentText": "Coca-Cola is the number 1 soft drink in the world!",
-        "time": "23:54",
-        "photoUrl": "https://www.foodnavigator-asia.com/var/wrbm_gb_food_pharma/storage/images/_aliases/wrbm_large/publications/food-beverage-nutrition/foodnavigator-asia.com/headlines/business/coca-cola-stands-firm-amid-criticism-of-human-rights-violations-in-south-east-asia/8624735-1-eng-GB/Coca-Cola-stands-firm-amid-criticism-of-human-rights-violations-in-South-East-Asia.jpg",
-        "description": "For people addicted to Coca-Cola"
-      },
-      {
-        "groupId": "10028",
-        "groupName": "Addicted to Coca-Cola",
-        "mostRecentText": "Coca-Cola is the number 1 soft drink in the world!",
-        "time": "23:54",
-        "photoUrl": "https://www.foodnavigator-asia.com/var/wrbm_gb_food_pharma/storage/images/_aliases/wrbm_large/publications/food-beverage-nutrition/foodnavigator-asia.com/headlines/business/coca-cola-stands-firm-amid-criticism-of-human-rights-violations-in-south-east-asia/8624735-1-eng-GB/Coca-Cola-stands-firm-amid-criticism-of-human-rights-violations-in-South-East-Asia.jpg",
-        "description": "For people addicted to Coca-Cola"
-      },
-      {
-        "groupId": "10029",
-        "groupName": "Addicted to Coca-Cola",
-        "mostRecentText": "Coca-Cola is the number 1 soft drink in the world!",
-        "time": "23:54",
-        "photoUrl": "https://www.foodnavigator-asia.com/var/wrbm_gb_food_pharma/storage/images/_aliases/wrbm_large/publications/food-beverage-nutrition/foodnavigator-asia.com/headlines/business/coca-cola-stands-firm-amid-criticism-of-human-rights-violations-in-south-east-asia/8624735-1-eng-GB/Coca-Cola-stands-firm-amid-criticism-of-human-rights-violations-in-South-East-Asia.jpg",
-        "description": "For people addicted to Coca-Cola"
-      },
-      {
-        "groupId": "10030",
-        "groupName": "Addicted to Coca-Cola",
-        "mostRecentText": "Coca-Cola is the number 1 soft drink in the world!",
-        "time": "23:54",
-        "photoUrl": "https://www.foodnavigator-asia.com/var/wrbm_gb_food_pharma/storage/images/_aliases/wrbm_large/publications/food-beverage-nutrition/foodnavigator-asia.com/headlines/business/coca-cola-stands-firm-amid-criticism-of-human-rights-violations-in-south-east-asia/8624735-1-eng-GB/Coca-Cola-stands-firm-amid-criticism-of-human-rights-violations-in-South-East-Asia.jpg",
-        "description": "For people addicted to Coca-Cola"
-      },
-      {
-        "groupId": "10031",
-        "groupName": "Addicted to Coca-Cola",
-        "mostRecentText": "Coca-Cola is the number 1 soft drink in the world!",
-        "time": "23:54",
-        "photoUrl": "https://www.foodnavigator-asia.com/var/wrbm_gb_food_pharma/storage/images/_aliases/wrbm_large/publications/food-beverage-nutrition/foodnavigator-asia.com/headlines/business/coca-cola-stands-firm-amid-criticism-of-human-rights-violations-in-south-east-asia/8624735-1-eng-GB/Coca-Cola-stands-firm-amid-criticism-of-human-rights-violations-in-South-East-Asia.jpg",
-        "description": "For people addicted to Coca-Cola"
-      },
-      {
-        "groupId": "10032",
-        "groupName": "Addicted to Coca-Cola",
-        "mostRecentText": "Coca-Cola is the number 1 soft drink in the world!",
-        "time": "23:54",
-        "photoUrl": "https://www.foodnavigator-asia.com/var/wrbm_gb_food_pharma/storage/images/_aliases/wrbm_large/publications/food-beverage-nutrition/foodnavigator-asia.com/headlines/business/coca-cola-stands-firm-amid-criticism-of-human-rights-violations-in-south-east-asia/8624735-1-eng-GB/Coca-Cola-stands-firm-amid-criticism-of-human-rights-violations-in-South-East-Asia.jpg",
-        "description": "For people addicted to Coca-Cola"
-      },
-      {
-        "groupId": "10033",
-        "groupName": "Addicted to Coca-Cola",
-        "mostRecentText": "Coca-Cola is the number 1 soft drink in the world!",
-        "time": "23:54",
-        "photoUrl": "https://www.foodnavigator-asia.com/var/wrbm_gb_food_pharma/storage/images/_aliases/wrbm_large/publications/food-beverage-nutrition/foodnavigator-asia.com/headlines/business/coca-cola-stands-firm-amid-criticism-of-human-rights-violations-in-south-east-asia/8624735-1-eng-GB/Coca-Cola-stands-firm-amid-criticism-of-human-rights-violations-in-South-East-Asia.jpg",
-        "description": "For people addicted to Coca-Cola"
+        "photoUrl": "https://thumbs.dreamstime.com/b/can-pepsi-cola-london-uk-june-th-over-plain-white-background-th-june-product-produced-manufactured-pepsico-73246795.jpg",
+        "description": "For people who clearly have no taste"
       },
     ];
 
@@ -153,6 +100,7 @@ class _GroupsPageState extends State<GroupsPage> {
     return Padding(
           padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
           child: TextField(
+            onChanged: (value) => _runFilter(value),
             decoration: InputDecoration(
                 hintText: "Search...",
                 hintStyle: TextStyle(
@@ -175,6 +123,19 @@ class _GroupsPageState extends State<GroupsPage> {
             ),
           ),
         );
+  }
+
+  void _runFilter(String filter) {
+    List<ChatGroup> results = [];
+    if (filter.isEmpty) {
+      results = allGroups;
+    } else {
+      results = allGroups.where((group) => group.groupName.toLowerCase().contains(filter.toLowerCase())).toList();
+    }
+
+    setState(() {
+      groups = results;
+    });
   }
 
   Widget createAddGroupButton() {
