a9e3cf8f6fd3d46fb994ef83c3555634f48c6312
Jotham Wong <45916998+JothamWong@users.noreply.github.com>
2022-06-18 22:08:33 +0800

    refactored the code directory

--- mobile/lib/pages/groups_page.dart
+++ mobile/lib/pages/groups_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mobile/groups/pages/chat_page.dart';
+import 'package:mobile/pages/chat_page.dart';
 import 'package:mobile/services/group.dart';
 
 import '../model/chat_group.dart';
