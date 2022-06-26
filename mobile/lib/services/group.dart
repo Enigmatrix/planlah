a898a76fa05817565a425af45ff730d6eecede6b
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-06-27 01:01:40 +0800

    merge

--- mobile/lib/services/group.dart
+++ mobile/lib/services/group.dart
@@ -6,5 +6,13 @@ class GroupService extends BaseConnect {
   Future<Response<List<GroupSummaryDto>?>> getGroup() async => await get('/groups/all',
       decoder: decoderForList(GroupSummaryDto.fromJson));
 
-  // Future<Response<List<>>>
+  Future<Response<List<GroupInviteDto>?>> getGroupInvites(num groupId) async => await get('/groups/invites', query: { "groupId": groupId },
+      decoder: decoderForList(GroupInviteDto.fromJson));
+
+  Future<Response<GroupInviteDto?>> createGroupInvite(CreateGroupInviteDto dto) async => await post('/groups/invites/create', dto.toJson(),
+      decoder: decoderFor(GroupInviteDto.fromJson));
+
+  Future<Response<void>> invalidateGroupInvite(num inviteId) async => await put('/groups/invites/invalidate', { 'inviteId': inviteId });
+
+  Future<Response<GroupSummaryDto?>> joinByInvite(String inviteId) async => await get('/groups/join/$inviteId', decoder: decoderFor(GroupSummaryDto.fromJson));
 }
