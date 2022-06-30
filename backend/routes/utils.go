a3caa2123f845aa43fb70b050b77398612fd8628
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-06-30 23:22:38 +0800

    feat(refactor): new error types

--- backend/routes/utils.go
+++ backend/routes/utils.go
@@ -5,16 +5,37 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/samber/lo"
 	"io"
 	"net/http"
 )
 
+type ErrorKind struct {
+	Kind string `json:"kind" binding:"required"`
+}
+
 type ErrorMessage struct {
+	ErrorKind
 	Message string `json:"message" binding:"required"`
 }
 
-func NewErrorMessage(msg string) ErrorMessage {
-	return ErrorMessage{Message: msg}
+type ValidationErrorMessage struct {
+	ErrorKind
+	Fields []string `json:"fields" binding:"required"`
+}
+
+var friendlyError = ErrorKind{Kind: "FRIENDLY_ERROR"}
+var genericError = ErrorKind{Kind: "GENERIC_ERROR"}
+var validationFieldsError = ErrorKind{Kind: "VALIDATION_FIELDS_ERROR"}
+var validationMissingBodyError = ErrorKind{Kind: "VALIDATION_MISSING_BODY_ERROR"}
+var validationGenericError = ErrorKind{Kind: "VALIDATION_GENERIC_ERROR"}
+
+func FailWithError(ctx *gin.Context, err error) {
+	FailWithMessage(ctx, err.Error())
+}
+
+func FailWithMessage(ctx *gin.Context, msg string) {
+	ctx.JSON(http.StatusBadRequest, ErrorMessage{ErrorKind: friendlyError, Message: msg})
 }
 
 // Body binds the POST body of the request to the DTO, and gives a
@@ -61,22 +82,18 @@ func Query[T any](ctx *gin.Context, dto *T) error {
 }
 
 func processValidationError[T any](ctx *gin.Context, err error) error {
-	// TODO make this only run in debug
 	var validationErrors validator.ValidationErrors
-
 	if errors.As(err, &validationErrors) {
-		validationMessage := make([]string, len(validationErrors))
-		for i, fieldError := range validationErrors {
-			validationMessage[i] = fieldError.Error()
-		}
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "validation", "validation": validationMessage})
+		fields := lo.Map(validationErrors, func(fld validator.FieldError, _ int) string {
+			return fld.Error()
+		})
+		ctx.JSON(http.StatusBadRequest, ValidationErrorMessage{ErrorKind: validationFieldsError, Fields: fields})
 	} else if err == io.EOF {
 		var d T
 		message := fmt.Sprintf("Body of type %T expected", d)
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "missingBody", "message": message})
+		ctx.JSON(http.StatusBadRequest, ErrorMessage{ErrorKind: validationMissingBodyError, Message: message})
 	} else {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "unknown", "message": err.Error()})
+		ctx.JSON(http.StatusBadRequest, ErrorMessage{ErrorKind: validationGenericError, Message: err.Error()})
 	}
 	return err
-
 }
