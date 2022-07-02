c68a240aa26d08fd883b2f3511a574d5b08939f4
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-07-03 01:19:32 +0800

    feat(test): initial integration test

--- backend/integration_tests/data_test.go
+++ backend/integration_tests/data_test.go
@@ -5,6 +5,9 @@ import (
 	"syscall"
 	"testing"
 
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+
 	"github.com/joho/godotenv"
 	"planlah.sg/backend/data"
 	"planlah.sg/backend/utils"
@@ -12,58 +15,80 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type dataE2ETestSuite struct {
+type DataIntegrationTestSuite struct {
 	suite.Suite
-	db data.Database
+	db     *data.Database
+	conn   *gorm.DB
+	config *utils.Config
 }
 
 func TestDataE2ETestSuite(t *testing.T) {
-	suite.Run(t, &dataE2ETestSuite{})
+	suite.Run(t, &DataIntegrationTestSuite{})
 }
 
-func (s *dataE2ETestSuite) SetupSuite() {
+func (s *DataIntegrationTestSuite) SetupSuite() {
 	s.Require().NoError(godotenv.Load("../.test.env"))
 
 	config, err := utils.NewConfig()
 	s.Require().NoError(err)
+	s.config = config
+}
 
-	conn, err := data.NewPostgresGormDatabaseConnection(config)
+func (s *DataIntegrationTestSuite) TearDownSuite() {
+	p, _ := os.FindProcess(syscall.Getpid())
+	_ = p.Signal(syscall.SIGINT) // ignored on Windows
+}
+
+func (s *DataIntegrationTestSuite) SetupTest() {
+	// reinit database
+	conn, err := data.NewDatabaseConnection(s.config, zap.NewNop())
 	s.Require().NoError(err)
 
 	db := data.NewDatabase(conn)
+
 	s.db = db
+	s.conn = conn
 }
 
-func (s *dataE2ETestSuite) TearDownSuite() {
-	p, _ := os.FindProcess(syscall.Getpid())
-	p.Signal(syscall.SIGINT)
-}
-
-func (s *dataE2ETestSuite) SetupTest() {
-	// reinit database
-}
-
-func (s *dataE2ETestSuite) TearDownTest() {
+func (s *DataIntegrationTestSuite) TearDownTest() {
 	// destroy database
+	s.Require().NoError(s.conn.Exec(`
+		DROP SCHEMA public CASCADE;
+		CREATE SCHEMA public;
+		GRANT ALL ON SCHEMA public TO postgres;
+		GRANT ALL ON SCHEMA public TO public;
+	`).Error)
 }
 
-func (s *dataE2ETestSuite) TestInit() {
+func (s *DataIntegrationTestSuite) Test_InitMigrate() {
 	s.NotNil(s.db)
 	// there should be 7 Users
-	s.NotNil(s.db.GetUser(1))
-	s.NotNil(s.db.GetUser(2))
-	s.NotNil(s.db.GetUser(3))
-	s.NotNil(s.db.GetUser(4))
-	s.NotNil(s.db.GetUser(5))
-	s.NotNil(s.db.GetUser(6))
-	s.NotNil(s.db.GetUser(7))
-	s.Nil(s.db.GetUser(8))
+	for i := 1; i <= 7; i++ {
+		user, err := s.db.GetUser(uint(i))
+		s.Equal(uint(i), user.ID)
+		s.Nil(err)
+	}
+	_, err := s.db.GetUser(uint(8))
+	s.ErrorIs(err, data.EntityNotFound)
 
 	// and only 5 groups
-	s.NotNil(s.db.GetGroup(1))
-	s.NotNil(s.db.GetGroup(2))
-	s.NotNil(s.db.GetGroup(3))
-	s.NotNil(s.db.GetGroup(4))
-	s.NotNil(s.db.GetGroup(5))
-	s.Nil(s.db.GetGroup(6))
+	for i := 1; i <= 5; i++ {
+		grp, err := s.db.GetGroup(0, uint(i))
+		s.Equal(uint(i), grp.ID)
+		s.Nil(err)
+	}
+	_, err = s.db.GetGroup(0, uint(6))
+	s.ErrorIs(err, data.EntityNotFound)
+}
+
+func (s *DataIntegrationTestSuite) Test_AddGroup() {
+	group := data.Group{
+		Name:        "groupName",
+		Description: "groupDescription",
+		ImageLink:   "imageLink",
+	}
+	err := s.db.CreateGroup(&group)
+	s.Require().NoError(err)
+
+	s.NotEqual(0, group.ID)
 }
