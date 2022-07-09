a5addb3cd0024fd8252152aef107c376fc76999c
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-07-09 20:08:41 +0800

    feat(mobile): change frontend dto & models

--- mobile/lib/dto/outing.dart
+++ mobile/lib/dto/outing.dart
@@ -1,6 +1,5 @@
 import 'package:mobile/dto/place.dart';
 import 'package:mobile/dto/user.dart';
-import 'package:mobile/dto/outing_step.dart';
 
 class CreateOutingDto {
   String name;
@@ -22,6 +21,66 @@ class CreateOutingDto {
   };
 }
 
+class OutingStepVoteDto {
+  bool vote;
+  UserSummaryDto userSummaryDto;
+
+  OutingStepVoteDto(this.vote, this.userSummaryDto);
+
+  OutingStepVoteDto.fromJson(Map<String, dynamic> json):
+      vote = json["vote"],
+      userSummaryDto = UserSummaryDto.fromJson(json["user"]);
+
+  static List<OutingStepVoteDto> fromJsonToList(List<dynamic> json) {
+    List<OutingStepVoteDto> result = [];
+    for (int i = 0; i < json.length; i++) {
+      result.add(OutingStepVoteDto.fromJson(json[i]));
+    }
+    return result;
+  }
+}
+
+class OutingStepDto {
+  int id;
+  Place place;
+  String description;
+  String start;
+  String end;
+  List<OutingStepVoteDto> outingStepVoteDtos;
+  String voteDeadline;
+
+
+  OutingStepDto(
+      this.id,
+      this.place,
+      this.description,
+      this.start,
+      this.end,
+      this.outingStepVoteDtos,
+      this.voteDeadline);
+
+  OutingStepDto.fromJson(Map<String, dynamic> json):
+      id = json["id"],
+      place = Place.fromJson(json["place"]),
+      description = json["description"],
+      start = json["start"],
+      end = json["end"],
+      outingStepVoteDtos = OutingStepVoteDto.fromJsonToList(json["votes"]),
+      voteDeadline = json["voteDeadline"];
+
+  static List<List<OutingStepDto>> fromJsonToList(List<dynamic> json) {
+    List<List<OutingStepDto>> result = [];
+    for(int i = 0; i < json.length; i++) {
+      List<OutingStepDto> tmp = [];
+      for(int j = 0; j < json[i].length; j++) {
+        tmp.add(OutingStepDto.fromJson(json[i][j]));
+      }
+      result.add(tmp);
+    }
+    return result;
+  }
+}
+
 class OutingDto {
   int id;
   String name;
@@ -29,11 +88,11 @@ class OutingDto {
   int groupId;
   String start;
   String end;
-  List<List<OutingStepDto>> steps;
+  List<List<OutingStepDto>> outingStepDto;
 
 
   OutingDto(this.id, this.name, this.description, this.groupId, this.start,
-      this.end, this.steps);
+      this.end, this.outingStepDto);
 
   OutingDto.fromJson(Map<String, dynamic> json)
       :  id = json['id'],
@@ -42,7 +101,15 @@ class OutingDto {
          groupId = json["groupId"],
          start = json["start"],
          end = json["end"],
-         steps = OutingStepDto.fromJsonToList(json["steps"]);
+         outingStepDto = OutingStepDto.fromJsonToList(json["steps"]);
+
+  int getSize() {
+    return outingStepDto.length;
+  }
+
+  int getCurrentOuting() {
+    return getSize() - 1;
+  }
 }
 
 class GetActiveOutingDto {
