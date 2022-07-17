7026d7b5241f2c5d2782f07cca5837832ba5d48d
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-07-17 20:36:04 +0800

    feat(backend): create post

--- backend/routes/posts.go
+++ backend/routes/posts.go
@@ -2,14 +2,17 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/juju/errors"
 	"github.com/samber/lo"
 	"net/http"
 	"planlah.sg/backend/data"
+	"planlah.sg/backend/services"
 	"time"
 )
 
 type PostsController struct {
 	BaseController
+	ImageService services.ImageService
 }
 
 type ListPostsDto struct {
@@ -37,6 +40,11 @@ func ToPostDto(post data.Post) PostDto {
 		ImageLink:  post.ImageLink,
 		PostedAt:   post.PostedAt,
 	}
+}
+
+type CreatePostDto struct {
+	Text         string `form:"text" binding:"required"`
+	OutingStepID uint   `form:"outingStepId" binding:"required"`
 }
 
 // GetAll godoc
@@ -67,7 +75,73 @@ func (ctr *PostsController) GetAll(ctx *gin.Context) {
 	}))
 }
 
+// CreatePost godoc
+// @Summary Create a new Post
+// @Description Create a new Post given a `CreatePostDto`.
+// @Param form formData CreatePostDto true "Details of newly created post"
+// @Param        image  formData  file  true  "Post Image"
+// @Accept       multipart/form-data
+// @Tags Posts
+// @Success 200
+// @Failure 400 {object} ErrorMessage
+// @Failure 401 {object} services.AuthError
+// @Router /api/posts/create [post]
+func (ctr *PostsController) CreatePost(ctx *gin.Context) {
+	userId := ctr.AuthUserId(ctx)
+
+	var dto CreatePostDto
+	if Form(ctx, &dto) {
+		return
+	}
+
+	// maybe only allow upto a certain file size in meta (_ in below line)
+	file, _, err := ctx.Request.FormFile("image")
+	if err != nil {
+		FailWithMessage(ctx, "image file field missing")
+		return
+	}
+
+	imageUrl, err := ctr.ImageService.UploadPostImage(file)
+	if err != nil {
+		// TODO handle this
+		return
+	}
+
+	o, err := ctr.Database.GetOutingAndGroupForOutingStep(dto.OutingStepID)
+	if errors.Is(err, data.EntityNotFound) {
+		FailWithMessage(ctx, "outing step not found")
+		return
+	} else if err != nil {
+		handleDbError(ctx, err)
+		return
+	}
+
+	grpMember := ctr.AuthGroupMember(ctx, o.GroupID)
+	if grpMember == nil {
+		return
+	}
+
+	post := data.Post{
+		UserID:       userId,
+		OutingStepID: dto.OutingStepID,
+		Text:         dto.Text,
+		ImageLink:    imageUrl,
+		PostedAt:     time.Now().In(time.UTC),
+	}
+	err = ctr.Database.CreatePost(&post)
+	if err != nil {
+		handleDbError(ctx, err)
+		return
+	}
+
+	err = ctr.Hub.SendToFriends(userId, services.NewPostUpdate(userId))
+	handleHubError(ctr.Logger, err)
+
+	ctx.Status(http.StatusOK)
+}
+
 func (ctr *PostsController) Register(router *gin.RouterGroup) {
 	posts := router.Group("posts")
 	posts.GET("all", ctr.GetAll)
+	posts.POST("create", ctr.CreatePost)
 }
